a3f32c14d0ba209a401428d8992e539d8d2c9f1c
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-09-23 21:12:34 -0500

    cleanup

--- src/pages/whitepapers/whitepapers.ts
+++ src/pages/whitepapers/whitepapers.ts
@@ -8,14 +8,13 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class WhitepapersPage {
 
-  whitepapers: Array<{ no: number, title: string, note: string,link:string}>;
+  whitepapers: Array<{ no: number, title: string, note: string,logo:string,icon:string,link:string}>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.whitepapers = [{no:1,title:"Overview of Security Processes",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Security/AWS_Security_Best_Practices.pdf"},
-    {no:2,title:"Storage Options in the Cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Storage/AWS%20Storage%20Services%20Whitepaper-v9.pdf"},
-    {no:3,title:"Fault Tolerant Apps in the cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/aws-building-fault-tolerant-applications.pdf"},
-    {no:4,title:"Overview of AWS",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/aws-overview.pdf"},
-    {no:5,title:"Compliance Whitepaper",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/compliance/AWS_Risk_and_Compliance_Whitepaper.pdf"},
-    {no:6,title:"Architecting for the AWS Cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/AWS_Cloud_Best_Practices.pdf"}];
+    this.whitepapers = [{no:1,title:"Overview of Amazon Web Services",logo:"assets/img/pdf-icon.ico",note:"sdfsdfsd",icon:"md-download",link:"https://d0.awsstatic.com/whitepapers/aws-overview.pdf"},
+    {no:2,title:"Architecting for the Cloud: AWS Best Practices",logo:"assets/img/pdf-icon.ico",icon:"md-download",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/AWS_Cloud_Best_Practices.pdf"},
+    {no:3,title:"How AWS Pricing Works",note:"sdfsdfsd",logo:"assets/img/pdf-icon.ico",icon:"md-download",link:"https://d0.awsstatic.com/whitepapers/aws_pricing_overview.pdf"},
+    {no:4,title:"The Total Cost of (Non) Ownership of Web Applications in the Cloud",logo:"assets/img/pdf-icon.ico",note:"sdfsdfsd",icon:"md-download",link:"https://d1.awsstatic.com/whitepapers/aws-tco-web-applications.pdf"},
+    {no:5,title:"Compare AWS Support Plans",note:"sdfsdfsd",icon:"paper-plane",logo:"assets/img/html.png",link:"https://aws.amazon.com/premiumsupport/compare-plans/"}];
   }
 
   ionViewDidLoad() {
